776460146ffd04a1e21dcf8db527c633168f061a
Kim, Jang-hwan <janghwan@gmail.com>
2013-12-02 18:20:40 +0900

    add basic codes to get it work

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
-import oauth2 as oauth
+import os
+import json
 import urlparse, urllib
+import oauth2 as oauth
 
 app_id = 'ecogwiki-jangxyz'
 url = 'http://ecogwiki-jangxyz.appspot.com/ecogwiki/client/example-1?_type=json'
@@ -11,7 +13,11 @@ ACCESS_TOKEN_URL  = "https://%s.appspot.com/_ah/OAuthGetAccessToken"  % app_id
 
 client_id     = '576416393937-rmcaesbkv0rfdcq71l5ol9p3sbmv1qf9.apps.googleusercontent.com'
 client_secret = 'f_7_soOcc_SZhlDzLfUB0d-t'
+consumer = oauth.Consumer(client_id, client_secret)
 
+
+# system editor
+editor = os.environ.get('EDITOR', 'vi')
 
 #
 def to_url(url, params={}):
@@ -176,9 +182,103 @@ def post(consumer, access_token, url):
     return content
 
 
-if __name__ == '__main__':
-    consumer = oauth.Consumer(client_id, client_secret)
+#
+#
+#
 
+class EcogWiki(object):
+    def __init__(self, url, access_token=None):
+        self.url = url # 'http://ecogwiki-jangxyz.appspot.com'
+        self.set_access_token(access_token)
+
+    def auth(self):
+        pass
+
+    def set_access_token(self, access_token):
+        self.access_token = access_token
+        self.client = oauth.Client(consumer, access_token)
+
+    def _request(self, url, method='GET', format=None, body='', headers=None):
+        params = {
+            'oauth_version': '2.0',
+        }
+        if format:
+            params['_type'] = format
+        resp, content = self.client.request(to_url(url, params), method, body=body)
+        if resp['status'] != '200':
+            raise Exception("Invalid response %s." % resp['status'])
+        return resp, content
+
+    def _highlight(self, text, format='markdown'):
+        import pprint
+        return pprint.pformat(text)
+        #from pygments import highlight
+        #from pygments.formatters import Terminal256Formatter
+        #from pygments.lexers import get_lexer_by_name
+
+    def list(self):
+        ''' shorthand for GET /sp.index?_type=atom '''
+        url = urllib.basejoin(self.url, 'sp.index')
+        resp, content = self._request(url, format='atom')
+        print self._highlight(content)
+
+    def all(self):
+        ''' shorthand for GET /sp.titles?_type=json '''
+        url = urllib.basejoin(self.url, 'sp.titles')
+        resp, content = self._request(url, format='json')
+        print self._highlight(json.loads(content))
+
+    def recent(self):
+        ''' shorthand for GET /sp.changes?_type=atom '''
+        url = urllib.basejoin(self.url, 'sp.changes')
+        resp, content = self._request(url, format='atom')
+        print self._highlight(content)
+
+    def get(self, title, format='json'):
+        url = urllib.basejoin(self.url, title)
+        resp, content = self._request(url, format=format)
+        if format == 'json':
+            content = json.loads(content)
+        print self._highlight(content)
+        return content
+
+    def cat(self, title):
+        ''' shorthand for GET TITLE?_type=rawbody '''
+        url = urllib.basejoin(self.url, title)
+        resp, content = self._request(url, format='rawbody')
+        print content
+        return content
+
+    def post(self, title, body, revision=None, comment=''):
+        if revision is None:
+            data = self.get(title)
+            revision = data['revision']
+        url = urllib.basejoin(self.url, title)
+        body = urllib.urlencode({
+            'title': title,
+            'body': body,
+            'revision': revision,
+            'comment': comment or 'post by ecogwiki client',
+        })
+        resp, content = self._request(url, format='json', method='POST', body=body)
+        print content
+        return content
+
+    def edit(self, title):
+        pass
+    
+    def search(self, title):
+        pass
+
+    def memo(self):
+        pass
+
+    def render(self, body, open=False):
+        ''' render markdown text into HTML '''
+        pass
+
+
+if __name__ == '__main__':
     # auth
     request_token  = step1_get_request_token(consumer)
     oauth_verifier = step2_user_authorization(request_token)
@@ -188,4 +288,5 @@ if __name__ == '__main__':
     content = get(consumer, access_token, url)
 
     print content
+
 
