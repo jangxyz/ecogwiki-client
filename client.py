e783f4d428b57f7967ccf045c5d27721371fa916
Kim, Jang-hwan <janghwan@gmail.com>
2013-12-02 14:27:23 +0900

    wrap up with functions

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import oauth2 as oauth
 import urlparse, urllib
 
-app_id = 'oauth.ecogwiki-jangxyz'
-url = 'http://oauth.ecogwiki-jangxyz.appspot.com/ecogwiki/client/example-1?_type=json'
-url = 'http://oauth.ecogwiki-jangxyz.appspot.com/ecogwiki/client/example-2?_type=json'
+app_id = 'ecogwiki-jangxyz'
+url = 'http://ecogwiki-jangxyz.appspot.com/ecogwiki/client/example-1?_type=json'
+url = 'http://ecogwiki-jangxyz.appspot.com/ecogwiki/client/example-2?_type=json'
 
 REQUEST_TOKEN_URL = "https://%s.appspot.com/_ah/OAuthGetRequestToken" % app_id
 AUTHORIZE_URL     = "https://%s.appspot.com/_ah/OAuthAuthorizeToken"  % app_id
@@ -11,12 +11,9 @@ ACCESS_TOKEN_URL  = "https://%s.appspot.com/_ah/OAuthGetAccessToken"  % app_id
 
 client_id     = '576416393937-rmcaesbkv0rfdcq71l5ol9p3sbmv1qf9.apps.googleusercontent.com'
 client_secret = 'f_7_soOcc_SZhlDzLfUB0d-t'
-consumer = oauth.Consumer(client_id, client_secret)
 
 
 #
-
-
 def to_url(url, params={}):
     """Serialize as a URL for a GET request."""
     base_url = urlparse.urlparse(url)
@@ -49,74 +46,102 @@ def to_url(url, params={}):
 
 
 
-# Step 1: Get a request token. This is a temporary token that is used for 
-# having the user authorize an access token and to sign the request to obtain 
-# said access token.
-client = oauth.Client(consumer)
-params = {
-    'oauth_version': '2.0',
-    'oauth_callback': 'oob',
-}
-resp, content = client.request(to_url(REQUEST_TOKEN_URL, params), "GET")
-if resp['status'] != '200':
-    raise Exception("Invalid response %s." % resp['status'])
+def step1_get_request_token(consumer):
+    # Step 1: Get a request token. This is a temporary token that is used for 
+    # having the user authorize an access token and to sign the request to obtain 
+    # said access token.
+    client = oauth.Client(consumer)
+    params = {
+        'oauth_version': '2.0',
+        'oauth_callback': 'oob',
+    }
+    resp, content = client.request(to_url(REQUEST_TOKEN_URL, params), "GET")
+    if resp['status'] != '200':
+        raise Exception("Invalid response %s." % resp['status'])
 
-request_token_dict = dict(urlparse.parse_qsl(content))
-request_token      = oauth.Token(request_token_dict['oauth_token'], request_token_dict['oauth_token_secret'])
+    request_token_dict = dict(urlparse.parse_qsl(content))
+    request_token      = oauth.Token(request_token_dict['oauth_token'], request_token_dict['oauth_token_secret'])
 
-print "Request Token:"
-print "    - oauth_token        = %s" % request_token.key
-print "    - oauth_token_secret = %s" % request_token.secret
-print 
+    print "Request Token:"
+    print "    - oauth_token        = %s" % request_token.key
+    print "    - oauth_token_secret = %s" % request_token.secret
+    print 
 
-
-print "Go to the following link in your browser:"
-print "%s?oauth_token=%s" % (AUTHORIZE_URL, request_token.key)
-print 
-
-# After the user has granted access to you, the consumer, the provider will
-# redirect you to whatever URL you have told them to redirect to. You can 
-# usually define this in the oauth_callback argument as well.
-accepted = 'n'
-while accepted.lower() == 'n':
-        accepted = raw_input('Have you authorized me? (y/n) ')
-
-oauth_verifier = raw_input('What is the PIN? ')
+    return request_token
 
 
-# Step 3: Once the consumer has redirected the user back to the oauth_callback
-# URL you can request the access token the user has approved. You use the 
-# request token to sign this request. After this is done you throw away the
-# request token and use the access token returned. You should store this 
-# access token somewhere safe, like a database, for future use.
-request_token.set_verifier(oauth_verifier)
 
-client = oauth.Client(consumer, request_token)
-params = {
-    'oauth_version': '2.0',
-}
-resp, content = client.request(to_url(ACCESS_TOKEN_URL, params), "POST")
+def step2_user_authorization(request_token):
+    # Step2
+    #
+    #
+    print "Go to the following link in your browser:"
+    print "%s?oauth_token=%s" % (AUTHORIZE_URL, request_token.key)
+    print 
 
-access_token_dict = dict(urlparse.parse_qsl(content))
-access_token      = oauth.Token(access_token_dict['oauth_token'], access_token_dict['oauth_token_secret'])
+    # After the user has granted access to you, the consumer, the provider will
+    # redirect you to whatever URL you have told them to redirect to. You can 
+    # usually define this in the oauth_callback argument as well.
+    accepted = 'n'
+    while accepted.lower() == 'n':
+            accepted = raw_input('Have you authorized me? (y/n) ')
 
-print "Access Token:"
-print "    - oauth_token        = %s" % access_token.key
-print "    - oauth_token_secret = %s" % access_token.secret
-print
-print "You may now access protected resources using the access tokens above." 
-print
+    oauth_verifier = raw_input('What is the PIN? ')
+
+    return oauth_verifier
+
+def step3_get_access_token(consumer, request_token, oauth_verifier):
+    # Step 3: Once the consumer has redirected the user back to the oauth_callback
+    # URL you can request the access token the user has approved. You use the 
+    # request token to sign this request. After this is done you throw away the
+    # request token and use the access token returned. You should store this 
+    # access token somewhere safe, like a database, for future use.
+    request_token.set_verifier(oauth_verifier)
+
+    client = oauth.Client(consumer, request_token)
+    params = {
+        'oauth_version': '2.0',
+    }
+    resp, content = client.request(to_url(ACCESS_TOKEN_URL, params), "POST")
+
+    access_token_dict = dict(urlparse.parse_qsl(content))
+    access_token      = oauth.Token(access_token_dict['oauth_token'], access_token_dict['oauth_token_secret'])
+
+    print "Access Token:"
+    print "    - oauth_token        = %s" % access_token.key
+    print "    - oauth_token_secret = %s" % access_token.secret
+    print
+    print "You may now access protected resources using the access tokens above." 
+    print
+
+    return access_token
 
 
-# Request resource
-client = oauth.Client(consumer, access_token)
-params = {
-    'oauth_version': '2.0',
-}
-resp, content = client.request(to_url(url,params), "GET")
-if resp['status'] != '200':
-    raise Exception("Invalid response %s." % resp['status'])
-print "Response Status Code: %s" % resp['status']
-print "Response body: %s" % content
+def request(consumer, access_token, url):
+    # Request resource
+    client = oauth.Client(consumer, access_token)
+    params = {
+        'oauth_version': '2.0',
+    }
+    resp, content = client.request(to_url(url, params), "GET")
+    if resp['status'] != '200':
+        raise Exception("Invalid response %s." % resp['status'])
+    print "Response Status Code: %s" % resp['status']
+    print "Response body: %s" % content
 
+    return content
+
+
+if __name__ == '__main__':
+    consumer = oauth.Consumer(client_id, client_secret)
+
+    # auth
+    request_token  = step1_get_request_token(consumer)
+    oauth_verifier = step2_user_authorization(request_token)
+    access_token   = step3_get_access_token(consumer, request_token, oauth_verifier)
+
+    # request resource
+    content = request(consumer, access_token, url)
+
+    print content
 
